5adf1d74045743f5d250ad453b7aedd0692848d6
OfficePlata <150496622+OfficePlata@users.noreply.github.com>
2026-02-03 14:11:24 +0900

    Update app.js

--- app.js
+++ app.js
@@ -556,7 +556,7 @@ async function confirmAndSubmitOrder() {
     });
     
     await sendThanksMessage(orderData);
-    showCustomAlert('注文完了', 'ご注文ありがとうございます！\nお店でお待ちしております。', () => liff.closeWindow());
+    showCustomAlert('注文完了', 'お店からの返信をもって注文完了です！\nお店からの返信を必ずご確認ください！', () => liff.closeWindow());
 
   } catch (err) {
     showCustomAlert('注文エラー', `通信エラーが発生しましたが、注文が送信された可能性があります。\n念のため店頭でご確認ください。\n(${err.message})`);
